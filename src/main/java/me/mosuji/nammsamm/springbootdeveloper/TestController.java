05a6998aa3b1692bc6d56350099a26865aa7326a
nammsamm <ahtnwl1004@gmail.com>
2024-03-21 19:36:50 +0900

    03장 스프링 부트3 구조 이해하기

--- src/main/java/me/mosuji/nammsamm/springbootdeveloper/TestController.java
+++ src/main/java/me/mosuji/nammsamm/springbootdeveloper/TestController.java
@@ -1,12 +1,1 @@
-package me.mosuji.nammsamm.springbootdeveloper;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TestController {
-    @GetMapping("/test") // http://localhost:8080/test 로 요청(GET)을 보내면 화면에 Hello World! 출력.
-    public String test(){
-        return "Hello World!";
-    }
-}
+package me.mosuji.nammsamm.springbootdeveloper;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.RestController;import java.util.List;@RestControllerpublic class TestController {    @Autowired // TestService 빈 주입    TestService testService;    @GetMapping("/test")    public List<Member> getAllMembers(){        List<Member> members = testService.getAllMembers();        return members;    }}
