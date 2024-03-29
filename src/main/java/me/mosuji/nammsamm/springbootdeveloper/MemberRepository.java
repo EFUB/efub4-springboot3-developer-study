cf06eda15b0171bef991cb9806857f26696b2395
nammsamm <ahtnwl1004@gmail.com>
2024-03-29 23:50:09 +0900

    기존 파일 삭제

--- src/main/java/me/mosuji/nammsamm/springbootdeveloper/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package me.mosuji.nammsamm.springbootdeveloper;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-// member 라는 이름의 테이블에 접근해서 Member 클래스에 매핑하는 구현체
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
