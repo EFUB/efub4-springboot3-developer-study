365a2bc0fe40779b9a40fd708d8ba6f82287c256
이여진 <yeojinlee1020@gmail.com>
2024-03-30 16:32:39 +0900

    06장 블로그 기획하고 API만들기

---
+++ src/main/java/me/yeojinLee/springbootdeveloper/BlogRepository.java
@@ -0,0 +1,7 @@
+package me.yeojinLee.springbootdeveloper;
+
+import me.yeojinLee.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
