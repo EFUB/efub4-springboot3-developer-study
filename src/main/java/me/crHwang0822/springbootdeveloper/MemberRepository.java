b77e07f9046371455808ada78a75f44700325e77
Chaerin Hwang <chaa00@naver.com>
2024-03-31 20:17:48 +0900

    06장 블로그 기획하고 API 만들기

--- src/main/java/me/crHwang0822/springbootdeveloper/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package me.crHwang0822.springbootdeveloper;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-}
---
+++ src/main/java/me/crHwang0822/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,19 @@
+package me.crHwang0822.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.crHwang0822.springbootdeveloper.domain.Article;
+import me.crHwang0822.springbootdeveloper.dto.AddArticleRequest;
+import me.crHwang0822.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor //final이 붙거나 @NotNull 이 붙은 필드의 생성자 추가
+@Service  //빈으로 등록
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    //블로그 글 추가 메서드
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
+}
