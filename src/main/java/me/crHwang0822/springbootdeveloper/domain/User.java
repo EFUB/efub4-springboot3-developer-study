20420b8bc1c2c3faeca1a32fe47ebde56c912ac0
Chaerin Hwang <chaa00@naver.com>
2024-05-19 23:56:49 +0900

    10장 OAuth2로 로그인/로그아웃 구현하기

--- src/main/java/me/crHwang0822/springbootdeveloper/domain/User.java
+++ src/main/java/me/crHwang0822/springbootdeveloper/domain/User.java
@@ -29,10 +29,14 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
     @Builder
-    public User(String email, String password, String auth){
+    public User(String email, String password, String nickname){
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
 
     //권한 반환
@@ -73,5 +77,11 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         //계정이 사용 가능한지 확인하는 로직이 들어갈 부분
         return true; //true → 사용 가능
+    }
+
+    //사용자 이름 변경
+    public User update(String nickname){
+        this.nickname = nickname;
+        return this;
     }
 }
