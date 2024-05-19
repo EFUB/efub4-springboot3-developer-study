0f0d18a55887f52d2f29bceb0819a3adc636f35c
이여진 <yeojinlee1020@gmail.com>
2024-05-19 19:25:44 +0900

    10장 OAuth2로 로그인/로그아웃 구현하기
    10장 OAuth2로 로그인/로그아웃 구현하기

--- src/main/java/me/yeojinLee/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/yeojinLee/springbootdeveloper/controller/BlogApiController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -21,8 +22,8 @@ public class BlogApiController {
     /* 글 생성 */
     @PostMapping("/api/articles")
     // @RequestBody로 본문 요청 값 매핑
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
-        Article savedArticle = blogService.save(request);
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request, Principal principal) {
+        Article savedArticle = blogService.save(request, principal.getName());
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
